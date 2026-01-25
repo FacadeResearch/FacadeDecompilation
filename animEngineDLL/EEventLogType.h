d33e5fac709bd15b350302432eed835ae50add4d
ohayo <9443476+ohayo@users.noreply.github.com>
2026-01-26 09:54:11 +1000

    Update EEventLogType.h

--- animEngineDLL/EEventLogType.h
+++ animEngineDLL/EEventLogType.h
@@ -1,5 +1,5 @@
 #pragma once
 
 enum EEventLogType {
-	ESpriteID = 0x10
+	ESpriteID = 2 // I don't actually know what value this is - placeholder for now, I do know it's defined - but the disassembly makes it look like magic numbers
 };
