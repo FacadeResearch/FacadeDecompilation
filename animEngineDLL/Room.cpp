a5180aebabfeb8b37b1a57af29671ab09ca5d5ff
bb <74681745+6x2@users.noreply.github.com>
2026-01-30 01:51:15 -0500

    Update Room.cpp

--- animEngineDLL/Room.cpp
+++ animEngineDLL/Room.cpp
@@ -1,10 +1,7 @@
 #include "Room.h"
 #include "Sprite.h"
 
-Sprite* CreatePanel(int spriteId, uint32_t nameId, int param3, int objectType, uint32_t param5,
-                    uint32_t param6, uint32_t param7, uint32_t param8, uint32_t param9, int param10,
-                    int param11, uint32_t color1, uint32_t texture, uint32_t color2, uint32_t posX,
-                    uint32_t posY, uint32_t posZ, int param16, int param17)
+Sprite* CreatePanel(int spriteId, uint32_t nameId, int param3, int objectType, uint32_t param5, uint32_t param6, uint32_t param7, uint32_t param8, uint32_t param9, int param10, int param11, uint32_t color1, uint32_t texture, uint32_t color2, uint32_t posX, uint32_t posY, uint32_t posZ, int param16, int param17)
 {
 	(void)spriteId;
 	(void)nameId;
